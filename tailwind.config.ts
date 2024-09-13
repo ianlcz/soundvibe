b4892395212c06afaeab431c2f3767f2c9594923
Yann Le Coz <ylcoz@icloud.com>
2024-09-13 10:25:35 +0200

    Initial commit from Create Next App

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/preline/preline.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [require("preline/plugin"), require("@tailwindcss/forms")],
+};
+export default config;
